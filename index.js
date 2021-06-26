ca753dbd6601e959d8f37767c5d71cd75aa44fa8
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-06-26 17:28:46 +0600

    server side done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express =require('express')
+const express = require('express')
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+const ObjectId = require('mongodb').ObjectId;
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
@@ -16,25 +16,43 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const blogsCollection = client.db("blogsdb").collection("blogs");
 
-  app.post('/addBlogs', (req, res) =>{
-      const blogData = req.body
-      blogsCollection.insertOne(blogData)
+  app.post('/addBlogs', (req, res) => {
+    const blogData = req.body
+    blogsCollection.insertOne(blogData)
       .then(result => {
-          console.log(result)
-          res.send(result.insertedCount > 0)
-        })
+        console.log(result)
+        res.send(result.insertedCount > 0)
+      })
+  })
+  //get a single item by id
+  app.get('/blog/:id', (req, res) => {
+    blogsCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((error, documents) => {
+        res.send(documents[0]);
+      })
   })
 
-  app.get('/blogs', (req, res) =>{
+  app.get('/blogs', (req, res) => {
     blogsCollection.find({})
-    .toArray((error, documents) =>{
-      res.send(documents)
-    })
+      .toArray((error, documents) => {
+        res.send(documents)
+      })
   })
+
+  app.delete('/deleteBlog/:id', (req, res) => {
+    console.log(req.params.id)
+    blogsCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        console.log(result)
+        res.send(result.deletedCount > 0);
+
+      })
+  })
+
   console.log('database connected successfully');
 });
 
-app.get('/', (req, res) =>{
-    res.send('Hello Infinity!')
+app.get('/', (req, res) => {
+  res.send('Hello Infinity!')
 })
 app.listen(port)
